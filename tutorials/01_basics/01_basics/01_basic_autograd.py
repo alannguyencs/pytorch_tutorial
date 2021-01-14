6fe65c4c12dc57154a434ff9d7abf67c94937ecc
alannguyencs <alannguyen.cs@gmail.com>
2021-01-14 10:16:04 +0800

    basic

--- tutorials/01_basics/01_basics/01_basic_autograd.py
+++ tutorials/01_basics/01_basics/01_basic_autograd.py
@@ -1,33 +1,47 @@
 import torch
 import torch.nn as nn
 
+"""
+Problem: Given two sets of inputs and targets, build a linear regression model to fit them
+Inputs: 10 samples of 3D vector
+Targets: 10 samples of 1D vector
+Linear model: targets = inputs * weight + bias (like Y = X * W + b)
+           --> optimize weight and bias
+reference: https://mccormickml.com/2014/03/04/gradient-descent-derivation/
+"""
 
-# Create tensors.
-x = torch.randn(10, 3)
-y = torch.randn(10, 2)
+inputs = torch.randn(10, 3)
+targets = torch.randn(10, 1)
 
-# Build a fully connected layer.
-linear = nn.Linear(3, 2)
-print ('w: ', linear.weight)
-print ('b: ', linear.bias)
+#===== Setup model =========
+linear = nn.Linear(3, 1)   # 10x1 = 10x3 * 3x1
+print ('weight: ', linear.weight)
+print ('bias: ', linear.bias)
 
-# Build loss function and optimizer.
+"""
+Setup loss function and optimizer
+pred_0 = inputs * weight_0 + bias_0
+Loss:
+    L1 loss = |targets - pred_0|
+    MSELoss = (targets - pred_0)^2
+
+gradient descent: linear_new = linear_old - lr * gradient
+"""
 criterion = nn.MSELoss()
 optimizer = torch.optim.SGD(linear.parameters(), lr=0.01)
 
-
+#===== Model training =========
 # Forward pass.
-pred = linear(x)
+pred = linear(inputs)
 
-# Compute loss.
-loss = criterion(pred, y)
+# Backward pass = compute loss + compute gradients + update weights
+loss = criterion(pred, targets)
 print('loss: ', loss.item())
 
-# Backward pass
-optimizer.zero_grad()   #set optimizer's values to zero
-loss.backward()         #--> compute gradients of network params
+optimizer.zero_grad()   # set gradients to zero
+loss.backward()         # compute gradients
 print ('dL/dw: ', linear.weight.grad)
 print ('dL/db: ', linear.bias.grad)
 
-# 1-step gradient descent --> update param's weight
-optimizer.step()
+optimizer.step()   # update weights
+
--- tutorials/01_basics/01_basics/02_numpy_torch.py
+++ tutorials/01_basics/01_basics/02_numpy_torch.py
@@ -5,12 +5,12 @@ import numpy as np
 # Create a numpy array.
 np_data = np.array([[1, 2], [3, 4]])
 
-# Convert the numpy array to a torch tensor.
+# Convert a numpy array to a torch tensor.
 torch_data = torch.from_numpy(np_data)
 
-# Convert the torch tensor to a numpy array.
+# Convert a torch tensor to a numpy array.
 np_data = torch_data.numpy()
 
-# Convert the GPU torch tensor to a numpy array.
+# Convert a GPU torch tensor to a numpy array.
 np_data = torch_data.data.cpu().numpy()
 
